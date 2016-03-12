34fc230a6de68bc01a4fbc9d6299a51f73047e9e
ASAPHAANING <haaning.frederik@gmail.com>
2016-03-12 16:45:15 +0100

    Forbindelse virker og tråd

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Thread queue = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    Document doc = null;
+                    try {
+                        doc = Jsoup.connect("https://vent.skat.dk/inqueue.aspx?c=skat&e=prod160304nemid&q=cb2c7f1a-8092-4b73-be90-299de8610f34&i=635933917086290542&cid=da-DK").get();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    String divContents = doc.select("#MainPart_lbUsersInLineAheadOfYou").first().text();
+                System.out.println("Antal brugere i kø foran dig: "Integer.parseInt(divContents));
+
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        queue.start();
+    }
+}

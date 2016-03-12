5f888d228cc8f05d601e162bd57c5965dfbab7e1
ASAPHAANING <haaning.frederik@gmail.com>
2016-03-12 16:54:36 +0100

    Song

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 public class Main {
 
+    private static final String SKATURL = "https://vent.skat.dk/inqueue.aspx?c=skat&e=prod160304nemid&q=cb2c7f1a-8092-4b73-be90-299de8610f34&i=635933917086290542&cid=da-DK";
+
     public static void main(String[] args) {
 
         Thread queue = new Thread(new Runnable() {
@@ -15,12 +17,12 @@ public class Main {
                 while(true) {
                     Document doc = null;
                     try {
-                        doc = Jsoup.connect("https://vent.skat.dk/inqueue.aspx?c=skat&e=prod160304nemid&q=cb2c7f1a-8092-4b73-be90-299de8610f34&i=635933917086290542&cid=da-DK").get();
+                        doc = Jsoup.connect(SKATURL).get();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     String divContents = doc.select("#MainPart_lbUsersInLineAheadOfYou").first().text();
-                System.out.println("Antal brugere i kø foran dig: "Integer.parseInt(divContents));
+                System.out.println("Antal brugere i kø foran dig: "+Integer.parseInt(divContents));
 
                     try {
                         Thread.sleep(10000);
